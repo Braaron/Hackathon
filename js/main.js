87f8ce90be7613c3ed9bac5081133981fbb676c7
David Simon <me@davidsimon.ch>
2014-08-10 17:24:01 +0200

    Bootstrapped Webpage layout (mendeley "clone")

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function (){
+   $('.slider').slick({
+       slidesToShow:3,
+       slidesToScroll:1,
+       autoplay: true,
+       autoplaySpeed:3000,
+       arrows:true,
+       dots:true
+   });
+});
